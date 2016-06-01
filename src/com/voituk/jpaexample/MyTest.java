8865d6b6837832666a3e9bde6ad7713df6eff75c
dn200978lak <aleksandr.lazarchuk@privatbank.ua>
2016-06-01 16:21:05 +0300

    master

--- src/com/voituk/jpaexample/MyTest.java
+++ src/com/voituk/jpaexample/MyTest.java
@@ -3,8 +3,15 @@ package com.voituk.jpaexample;
 public class MyTest {
 
 	public static void main(String[] args){
-	      Vehicle v = new Car();
-	       v.kmToMiles(10);
+		/**
+		 * При наследовании классов у которых есть статические методы (для статических методов) - полиморфизм НЕработает
+		 * то есть, при вызове статических методов будет вызван метод именно того класса тип которого был объявлен 
+		 */
+	    Vehicle v = new Car();
+	    v.kmToMiles(10);
+	       
+	    Car c = new Car();
+	    c.kmToMiles(10);
 	}
 	
 	
@@ -18,15 +25,15 @@ public class MyTest {
 }
 
 class Vehicle {
-//	public static void kmToMiles(int km){
-	public void kmToMiles(int km){
+	public static void kmToMiles(int km){
+//	public void kmToMiles(int km){
 		System.out.println("родительский");
 	}
 }
 
 class Car extends Vehicle {
-//	public static void kmToMiles(int km){
-	public void kmToMiles(int km){
+	public static void kmToMiles(int km){
+//	public void kmToMiles(int km){
 		System.out.println("дочерний");
 	}
 }
